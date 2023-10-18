c39d6d9eb514429abe6a0301959db8c6a665f5db
ximemunoz <92742110+ximemunoz@users.noreply.github.com>
2023-10-18 15:00:18 -0600

    Ximena'sPageFV

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -3,9 +3,44 @@ import "./contact.css"
 
 const Contact = () => {
   return (
-    <div>
-      Contact
-    </div>
+    <section className="contact container section" id='contact'>
+      <h2 className="section__title">Contacto</h2>
+
+      <div className="contact__container grid">
+        <div className="contact__info">
+          <h3 className="contact__title"> Hablemos de cualquier cosa... </h3>
+          <p className="contact__details">¿No te gustan los forms? Envíame un correo. </p>
+        </div>
+
+        <form action="" className="contact__form">
+          <div className="contact__form-group">
+            <div className="contact__form-div">
+              <input type="text" className="contact__form-input" placeholder='Escribe tu nombre'/>
+            </div>
+
+            <div className="contact__form-div">
+              <input type="email" className="contact__form-input" placeholder='Escribe tu email'/>
+            </div>
+          </div>
+
+          <div className="contact__form-div">
+              <input type="text" className="contact__form-input" placeholder='Escribe tu asunto'/>
+          </div>
+
+          <div className="contact__form-div contact__form-area">
+              <textarea 
+              name="" 
+              id="" 
+              cols="30" 
+              rows="10" 
+              className='contact__form-input' 
+              placeholder='Escribe tu mensaje'></textarea>
+          </div>
+          
+          <button className="btn">Enviar Mensaje</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- portfolio/src/components/home/Home.jsx
+++ portfolio/src/components/home/Home.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import "./home.css"
+import Me from "../../assets/avatar-1.jpg"
+import HeaderSocials from './HeaderSocials'
+import ScrollDown from './ScrollDown'
+import Shapes from './Shapes'
+
 const Home = () => {
   return (
     <div>
-      Home
+      <section className="home container" id="home">
+      
+        <div className="intro">
+          <img src={Me} alt="" className="home__img" />
+          <h1 className="home__name">Ximena Muñoz</h1>
+          <span className="home__education">Estudiante de Ingeniería en Animación en la Universidad Panamericana</span>
+
+          <HeaderSocials/>
+
+          <a href="#contact" className="btn">Contrátame</a>
+
+          <ScrollDown/>
+          
+        </div>
+      </section>
     </div>
   )
 }
