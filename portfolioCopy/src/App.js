d0c01bb8f1381122a0e37283b75795c78a1cfba6
ximemunoz <92742110+ximemunoz@users.noreply.github.com>
2023-10-18 16:39:00 -0600

    PabloUpdate

---
+++ portfolioCopy/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./App.css";
+import Sidebar from './components/sidebar/Sidebar';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Resume from './components/resume/Resume';
+import Portfolio from './components/portfolio/Portfolio';
+
+
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+      <main className='main'>
+        <Home/>
+        <About/>
+        <Resume/>
+        <Portfolio/>
+      </main>
+    </>
+  )
+}
+
+export default App;
---
+++ portfolioCopy/src/components/about/About.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import "./about.css"
+import Image from "../../assets/avatar-1.jpg"
+import AboutBox from './AboutBox'
+
+const About = () => {
+  return (
+    <section className="about container section" id="about">
+      <h2 className="section__title">Acerca de Mí</h2>
+
+      <div className="about__container grid">
+        <img src={Image} alt="" className="about__img"/>
+        <div className="about__data grid">
+          <div className="about__info">
+            <p className="about__description"> Mi nombre es Ximena, actualmente trabajo como becaria en el área de Coordinación de Ingenierías en donde administro el sitio web y apoyo en la logística del evento Open Doors. Tengo experiencia en diseño gráfico en con los programas Photoshop e Illustrator.</p>
+            <a href="https://drive.google.com/file/d/1_jKgJofgShOEz2dPQUiIFiGpCZujXUvn/view?usp=share_link" className="btn" target="_blank">Download CV</a>
+          </div>
+          <div className="about__skills grid">
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Código</h3>
+                <span className="skills__number">75%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills__percentage coding"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Resolución de Problemas</h3>
+                <span className="skills__number">80%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills__percentage ps"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Inglés</h3>
+                <span className="skills__number">100%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills__percentage en"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Trabajo en Equipo</h3>
+                <span className="skills__number">5%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills__percentage tw"></span>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <AboutBox/>
+    </section>
+  )
+}
+
+export default About
